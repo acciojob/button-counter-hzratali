d5674b2047761cb68819e887c8f0b6a854736c36
Hazrat Ali <hazrat8143@gmail.com>
2024-04-21 23:31:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [input, setInput] = useState(0);
+  const handleClick = () => {
+    setInput(input + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {input} times</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
